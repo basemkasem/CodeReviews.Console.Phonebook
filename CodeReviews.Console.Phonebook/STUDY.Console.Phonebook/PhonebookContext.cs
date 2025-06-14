14d7ed2fbaab6f2691bf81bd4261f4a6fef7cf60
basemkasem <69323267+basemkasem@users.noreply.github.com>
2025-06-15 02:39:07 +0300

    create a contact model and the db context

---
+++ CodeReviews.Console.Phonebook/STUDY.Console.Phonebook/PhonebookContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using STUDY.Console.Phonebook.Models;
+
+namespace STUDY.Console.Phonebook;
+
+public class PhonebookContext : DbContext
+{
+    public DbSet<Contact> Contacts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseSqlServer("Server=(localdb)\\MyLocalDB;Database=Phonebook;Trusted_Connection=True;");
+}
