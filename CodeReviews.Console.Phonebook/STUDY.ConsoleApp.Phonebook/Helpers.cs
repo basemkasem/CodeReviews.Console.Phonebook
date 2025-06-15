9026dc3a0037b5f470ad9626d018c01c335ac299
basemkasem <69323267+basemkasem@users.noreply.github.com>
2025-06-16 02:52:45 +0300

    Create a helper method to split the text of the choices for cleaner Ui

---
+++ CodeReviews.Console.Phonebook/STUDY.ConsoleApp.Phonebook/Helpers.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace STUDY.ConsoleApp.Phonebook;
+
+public static class Helpers
+{
+    public static List<string> SeparateString(this List<string> input)
+    {
+        var output = new List<string>();
+        StringBuilder builder = new StringBuilder();
+        
+        foreach (var item in input)
+        {
+            foreach (var c in item)
+            {
+                var character = c;
+                if (char.IsUpper(character) && builder.Length > 1)
+                {
+                    builder.Append(' ');
+                    character = char.ToLower(c);
+                }
+                builder.Append(character);
+            }
+            output.Add(builder.ToString());
+            builder.Clear();
+        }
+        return output;
+    }
+}
